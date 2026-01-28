aff26d205a830b6bd49c62af4bf764618b4d08e4
bogehben <shai_ming_kong@s2022.ssts.edu.sg>
2026-01-28 12:29:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -76,7 +76,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/5WDLRQ3VCdVrKw0njWe5E5?si=11a36144d1f04285", // Music streaming URL
+        musicUrl: "https://on.soundcloud.com/osyAsUdM83jc6DqV8m", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
